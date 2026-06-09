8dda685e6790352e6e00f33436e393dec7352a1e
Diego de Miranda <diego.demiranda@icloud.com>
2026-06-09 16:10:49 -0300

    Delete flask_test directory

--- flask_tutorial/config.py
+++
@@ -1,9 +0,0 @@
-"""
-config.py
-Instância compartilhada do SQLAlchemy.
-Criada aqui para evitar importações circulares entre app.py e models.py.
-""" 
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
